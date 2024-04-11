ca0914f6f29505239883ee2bdd70bdf9e638ebe4
tomasmajercik <tomas.majercik2@gmail.com>
2024-04-11 16:51:12 +0200

    added first functionality

--- main.c
+++ main.c
@@ -1,19 +1,76 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct vertex
+#define INFINITY 2147483646
+
+typedef struct GRAPH
 {
-    int name;
-    int status;
-    int before;
-    int pathLength;
+    int size;
+    int** weight;
+}Graph;
+
+Graph * createGraph(int size)
+{
+
+    Graph *graph = (Graph*)malloc(sizeof (Graph));
+    graph->size = size;
+
+    graph->weight = (int**)malloc(sizeof (int*) * size);
+    for (int i = 0; i < size; i++)
+    {
+        graph->weight[i] = (int*) malloc(sizeof (int) * size);
+        for (int j = 0; j < size; j++)
+        {
+            graph->weight[i][j] = INFINITY;
+        }
+    }
+    return graph;
+}
+
+void search(Graph *graph, int source, int destination)
+{
+    int *distance = (int*) malloc(sizeof (int) * graph->size);
+    int *predecessors = (int*) malloc(sizeof (int) * graph->size);
+    int *visited = (int*) malloc(sizeof (int) * graph->size);
 
 
-}VERTEX;
+
+}
 
 
 int main()
 {
-    printf("Hello, World!\n");
-    // please
+    int vertexes, edges;
+    scanf("%d %d", &vertexes, &edges);
+    Graph *graph = createGraph(vertexes);
+
+    for(int i = 1; i < edges; i++)
+    {
+        int source, destination, weight;
+        scanf(" (%d , %d , %d) ", &source, &destination, &weight);
+        graph->weight[source][destination] = weight;
+    }
+    printf("fertig");
+    char DO;
+
+    while((scanf(" %c", &DO) == 1))
+    {
+        switch(DO)
+        {
+            case 's':
+                int source, destination;
+                scanf(" %d %d", &source, &destination);
+                search(graph, source, destination);
+                break;
+        }
+    }
+
+    /////// Freeing stuff ///////
+    for (int i = 0; i < vertexes; i++)
+    {
+        free(graph->weight[i]);
+    }
+    free(graph->weight);
+    free(graph);
     return 0;
 }
