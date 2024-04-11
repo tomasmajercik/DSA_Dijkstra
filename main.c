90b56c30a0da9c67f84ad475f2fc1a09a74c66eb
tomasmajercik <tomas.majercik2@gmail.com>
2024-04-11 10:32:58 +0200

    first commit

---
+++ main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+typedef struct vertex
+{
+    int name;
+    int status;
+    int before;
+    int pathLength;
+
+
+}VERTEX;
+
+
+int main()
+{
+    printf("Hello, World!\n");
+    return 0;
+}
